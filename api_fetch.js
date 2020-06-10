68caa706030f47d99ad07c8ecd63ebb45ba687d1
Proladon <proladon@gmail.com>
2020-06-10 23:32:11 +0800

    404 image

--- api_fetch.js
+++ api_fetch.js
@@ -2,25 +2,42 @@
     var app = new Vue({
         el: "#app",
         data: {
+            none: false,
             user: '',
+            name: '',
+            github:'',
             img: '',
-            id:'',
+            id: '',
+            followers:'',
             created_date: '',
-            recent_update: ''
+            recent_update: '',
         },
         methods: {
-            getuser(user) {
+            getuser() {
+                user = this.user
+                if(this.user.trim('') == '') return
                 axios
                     .get(`https://api.github.com/users/${user}`)
                     .then((res) => {
-                        console.log(res.data.login);
+                        this.none = false
+                        this.name = res.data.avatar_url.login
                         this.img = res.data.avatar_url
                         this.id = res.data.id
+                        this.github = res.data.html_url
+                        this.followers = res.data.followers
                         this.created_date = res.data.created_at
                         this.recent_update = res.data.updated_at
                     })
                     .catch((err) => {
                         console.log(err);
+                        this.none = true
+                        this.name = this.user
+                        this.img = './src/kr2.gif'
+                        this.id = 'none'
+                        this.github = 'none'
+                        this.followers = 'none'
+                        this.created_date = 'none'
+                        this.recent_update = 'none'
                     });
             },
         },
